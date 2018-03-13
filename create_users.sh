56f15398d64ef48eba2671a3678dd5bb436736ca
Ruzhentsev Alexandr <webcode@pgallery.ru>
2018-03-13 13:12:37 +0300

    Modify

--- create_users.sh
+++ create_users.sh
@@ -1,19 +1,18 @@
 #!/bin/bash
 
+rabbitmqctl add_vhost /
+
 for user in `cat /opt/list_users`
 do
-
-    echo ${user}
 
     username=$(echo ${user}|awk -F":" {'print $1'})
     password=$(echo ${user}|awk -F":" {'print $2'})
     tag=$(echo ${user}|awk -F":" {'print $3'})
 
-    echo "Create user ${username} in tag ${tag}"
+    echo "Create user '${username}' in tag '${tag}'"
 
-#rabbitmqctl add_user ${username} ${password} ; \
-#rabbitmqctl set_user_tags ${username} ${tag} ; \
-#rabbitmqctl add_vhost / ; \
-#rabbitmqctl set_permissions -p / ${username} ".*" ".*" ".*" ; \
+    rabbitmqctl add_user ${username} ${password}
+    rabbitmqctl set_user_tags ${username} ${tag}
+    rabbitmqctl set_permissions -p / ${username} ".*" ".*" ".*"
+
 done
-
