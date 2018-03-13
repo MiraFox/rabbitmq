d4507eed586c0e9e5e22c147a10db3e1c2bdbca7
Ruzhentsev Alexandr <webcode@pgallery.ru>
2018-03-13 13:07:02 +0300

    Modify

--- create_users.sh
+++ create_users.sh
@@ -5,6 +5,12 @@ do
 
     echo ${user}
 
+    username=$(echo ${user}|awk -F":" {'print $1'})
+    password=$(echo ${user}|awk -F":" {'print $2'})
+    tag=$(echo ${user}|awk -F":" {'print $3'})
+
+    echo "Create user ${username} in tag ${tag}"
+
 #rabbitmqctl add_user ${username} ${password} ; \
 #rabbitmqctl set_user_tags ${username} ${tag} ; \
 #rabbitmqctl add_vhost / ; \
